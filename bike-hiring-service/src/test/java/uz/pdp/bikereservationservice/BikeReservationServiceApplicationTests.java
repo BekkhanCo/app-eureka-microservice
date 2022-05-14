d84b90b913e738493ebff3e5f890054994e5d1b8
imBOB <bekzodvaliev1@gmail.com>
2022-05-14 11:15:23 +0500

    added user service

---
+++ bike-hiring-service/src/test/java/uz/pdp/bikereservationservice/BikeReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.bikereservationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeReservationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
