ff48a8b52e3383a9748f0f0d63b4d7f46f2b3cad
imBOB <bekzodvaliev1@gmail.com>
2022-05-14 11:45:42 +0500

    added new modules

---
+++ bike-hiring-service/src/main/java/uz/pdp/bikehiringservice/BikeHiringServiceApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.bikehiringservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeHiringServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BikeHiringServiceApplication.class, args);
+    }
+
+}
